a9134bf29447fc75be3e1212563b71ea7489c358
Franchise <tylerreedsullivan@gmail.com>
2020-05-07 15:02:56 -0500

    updated color scheme, built out season page search, login dropdown beautified

--- routes/seasons.js
+++ routes/seasons.js
@@ -11,16 +11,34 @@ let express    = require("express");
 // ====================== Routes ==================================== //
 // ================================================================== //
 router.get("/seasons", function(req, res){
-    // Get all players from DB
-    Player.find({}, function(err, allPlayers){
-        if(err){
-            console.log(err);
-        } else {
-            res.render("seasons/index", {players: allPlayers, currentUser: req.user});
-        }
-    });
-})
+    if(req.query.search){
+        escapeRegex(req.query.search);
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Player.find({"season.year" : regex}, function(err, allPlayers){
+            if(err){
+                console.log(err);
+            } else {
+                if(allPlayers.length < 1) {
+                    return res.render("seasons/index", {players: allPlayers, "error": "No match! Please try again!"});
+                }
+                res.render("seasons/index",{players: allPlayers, currentUser: req.user});
+            }
+        });
+    } else {
+        // Get all players from DB
+        Player.find({}, function(err, allPlayers){
+            if(err){
+                console.log(err);
+            } else {
+                res.render("seasons/index",{players:allPlayers, currentUser: req.user});
+            }
+        });
+    }
+});
 
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 // ================================================================== //
 // ====================== Exports =================================== //
