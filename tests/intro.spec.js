fd05d7ff8a477fa2156e15d30df4c7f5fb326713
Franchise <tylerreedsullivan@gmail.com>
2021-12-11 14:29:06 -0600

    Initial testing setup.

--- tests/intro.spec.js
+++ tests/intro.spec.js
@@ -1,3 +1,8 @@
-describe('Player Model', () => {
+// Jest recognizes test files by: files with extension .test.js or .spec.js and all files inside of a __tests__ directory
 
+describe('Sample test', () => {
+    it('should test that true === true', () => {
+        expect(true).toBe(true)
+    })
 })
+
